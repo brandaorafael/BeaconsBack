83618036386a46f797fa22887cacdf321b602249
Rafael Brandão <r.augusto.brandao@gmail.com>
2017-11-24 16:33:56 -0200

    added config and cloud db

---
+++ config.js
@@ -0,0 +1,13 @@
+module.exports = function (){
+	return{
+		db : function(){
+			return {
+				dbuser: 'tcc',
+				dbpassword: 'tcc',
+				host: 'ds119476.mlab.com',
+				port: '19476',
+				db: 'tcc'
+			};
+		}
+	};
+}
--- index.js
+++ index.js
@@ -1,9 +1,11 @@
-var express = require('express');
-var path = require('path');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
+var express         = require('express');
+var path            = require('path');
+var app             = express();
+var http            = require('http').Server(app);
+var io              = require('socket.io')(http);
 var bodyParser      = require('body-parser');
+
+var config          = require('./config')();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -12,7 +14,7 @@ app.use(bodyParser.json());
 //Db
 var db = {};
 db.mongo = require('mongodb').MongoClient;
-db.mongoURI = 'mongodb://localhost:27017/beaconsBack';
+db.mongoURI = 'mongodb://' + config.db().dbuser + ':' + config.db().dbpassword + '@' + config.db().host + ':' + config.db().port + '/' + config.db().db;
 
 app.get('/', function(req, res){
   res.sendFile(__dirname + '/home.html');
@@ -21,6 +23,8 @@ app.get('/', function(req, res){
 app.get('/beacons', function(req, res){
   
   db.mongo.connect(db.mongoURI, function(err, dataBase){
+
+    if(err) throw err;
 
     dataBase.collection('beacons').find({}).toArray(function(err, beacons) {
       return res.send(beacons);
@@ -36,13 +40,13 @@ app.post('/beacons', function(req, res){
   var cellId = req.body.cellId;
   var beaconId = req.body.beaconId;
 
-
   db.mongo.connect(db.mongoURI, function(err, dataBase){
+
     dataBase.collection('beacons').update({cellId: cellId}, {cellId: cellId, beaconId: beaconId}, {upsert: true}, function(err, data){
       if(err) throw err;
 
       res.send(data);
-      
+
       io.emit('beacon', {cellId: cellId, beaconId: beaconId});
 
       return dataBase.close();
@@ -50,12 +54,6 @@ app.post('/beacons', function(req, res){
   })
 
 })
-
-// io.on('connection', function(socket){
-//   socket.on('beacon', function(beacon){
-//     io.emit('chat message', msg);
-//   });
-// });
 
 http.listen(3000, function(){
   console.log('listening on *:3000');
