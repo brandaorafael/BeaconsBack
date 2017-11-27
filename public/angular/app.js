1394f8825f06704226231052986346ba397f0032
Rafael Brandão <r.augusto.brandao@gmail.com>
2017-11-27 09:37:24 -0200

    more beautiful table and warning collumn

--- public/angular/app.js
+++ public/angular/app.js
@@ -15,39 +15,60 @@ angular.module('beacons', ['LocalStorageModule'])
 
                         $scope.beacons = [];
 
-      	  		var getBeaconName = function(beaconId){
+      	  		var getBeacon = function(beaconId){
 
                         	switch(beaconId) {
                         		case '08c7f93ee9697c35749f46413b764712':
-                        			return "Mint"
+                        			return {color: "Mint", location: "Sala"}
                         			break;
                         		case '246d7ead60b351d458d3c4c0887e3a14':
-                        			return "Blueberry"
+                        			return {color: "Blueberry", location: "Cozinha"}
                         			break;
                         		case 'ed74e21beab9d41e5ee96f54e9d6be2b':
-                        			return "Ice"
+                        			return {color: "Ice", location: "Área de Serviço"}
                         			break;
                         		case '0ae1cb5c7677108ec9085f4435085e1d':
-                        			return "Candy 1"
+                        			return {color: "Candy 1", location: "Sala de jantar"}
                         			break;
                         		case '0b080cb305bde1b8c504ea1ef2e4fb3c':
-                        			return "Beetroot 1"
+                        			return {color: "Beetroot 1", location: "Banheiro Suíte"}
                         			break;
                         		case '6639d16f2532af31ec99ecf23a3d0b18':
-                        			return "Beetroot 2"
+                        			return {color: "Beetroot 2", location: "Suíte"}
                         			break;
                         		case '6eeb5a738541d4e99a7cd9dd7acab539':
-                        			return "Lemon 1"
+                        			return {color: "Lemon 1", location: "Banheiro"}
                         			break;
                         		case 'c626263f8573361887f2885363964c1a':
-                        			return "Candy 2"
+                        			return {color: "Candy 2", location: "Escritório"}
                         			break;
                         		case 'fa30d761ceca937891b3c0bdf7fbf910':
-                        			return "Lemon 2"
+                        			return {color: "Lemon 2", location: "Varanda"}
                         			break;
                 				default:
-                    				return '??????'
+                    				return {color: "??????", location: "??????"}
             			}
+                        }
+
+                        var getPhone = function(udid){
+
+                              switch(udid) {
+                                    case 'cd2781c8a7fb8f0a':
+                                          return 'Celular Rebeca'
+                                          break;
+                                    default:
+                                          return udid
+                              }
+                        }
+
+                        var riskAreaBeacons = [
+                              '08c7f93ee9697c35749f46413b764712',
+                              'fa30d761ceca937891b3c0bdf7fbf910',
+                              'ed74e21beab9d41e5ee96f54e9d6be2b'
+                        ];
+
+                        $scope.isRiskArea = function(beaconId){
+                              return riskAreaBeacons.includes(beaconId);
                         }
 
                         $http.get('/env')
@@ -59,15 +80,17 @@ angular.module('beacons', ['LocalStorageModule'])
 
                               socket.on('beacon', function(beacon){
 
+                                    var beaconObject = getBeacon(beacon.beaconId);
+
+                                    var beaconHTML = {"cellId": getPhone(beacon.cellId) ,"beaconName": beaconObject.color, "beaconLocation": beaconObject.location, "beaconId": beacon.beaconId};
+
                                     if ($scope.beacons.filter(function(e) {return e.cellId == beacon.cellId}).length == 0) {
-                                          $scope.beacons.push({"cellId": beacon.cellId ,"beaconId": beacon.beaconId, "beaconName": getBeaconName(beacon.beaconId)});
+                                          $scope.beacons.push(beaconHTML);
                                     } else {
 
                                           var index = $scope.beacons.map(function(e) { return e.cellId; }).indexOf(beacon.cellId);
 
-                                          console.log(index);
-
-                                          $scope.beacons[index] = {"cellId": beacon.cellId ,"beaconId": beacon.beaconId, "beaconName": getBeaconName(beacon.beaconId)};
+                                          $scope.beacons[index] = beaconHTML;
                                     }
 
                                     $scope.$apply();
@@ -78,12 +101,14 @@ angular.module('beacons', ['LocalStorageModule'])
                         // $http.get($rootScope.api + 'beacons')
                         $http.get('/beacons')
                         .success(function(data){
-                              
-
-                              console.log(data);
 
                               for(i = 0; i < data.length; i++){
-                                    $scope.beacons.push({"cellId": data[i].cellId ,"beaconId": data[i].beaconId, "beaconName": getBeaconName(data[i].beaconId)});
+
+                                    var beaconObject = getBeacon(data[i].beaconId);
+
+                                    var beaconHTML = {"cellId": getPhone(data[i].cellId) ,"beaconName": beaconObject.color, "beaconLocation": beaconObject.location, "beaconId": data[i].beaconId};
+
+                                    $scope.beacons.push(beaconHTML);
                               }
 
                         })
